18a8db8ef26aaa3ca5a2587f8f635686e50e4033
Nicolás Hasbún A <nhasbun@gmail.com>
2022-05-30 01:35:40 -0400

    adding assembly productoEscalar16()

--- stm32_exercise/Core/Inc/asm_func.h
+++ stm32_exercise/Core/Inc/asm_func.h
@@ -7,3 +7,4 @@ void asm_svc (void);
 uint32_t asm_sum (uint32_t firstOperand, uint32_t secondOperand);
 void asm_zeros (uint32_t *vector, uint32_t longitud);
 void asm_productoEscalar32 (uint32_t * vectorIn, uint32_t * vectorOut, uint32_t longitud, uint32_t escalar);
+void asm_productoEscalar16 (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar);
